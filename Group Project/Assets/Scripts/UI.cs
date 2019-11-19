76e6db82c0bb4d14fab56bd5330e2220fbca0080
Phillip Murphy <56118445+philliprmurphy@users.noreply.github.com>
2019-11-18 23:40:27 -0500

    Finished basic UI, Work on Globals, Prefabs

---
+++ Group Project/Assets/Scripts/UI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public GameObject[] hearts;
+    public Text scoreText;
+    private Globals globals;
+
+    void Start()
+    {
+        globals = GameObject.FindObjectOfType<Globals>();
+
+        //Invoke("TestUI", 2.0f); // Testing UI
+    }
+
+    // Method purely for testing UI elements. Should change them every 2 seconds
+    void TestUI()
+    {
+        globals.changePlayerHealth(-4);
+        globals.changePlayerScore(4);
+        Invoke("TestUI", 2.0f);
+    }
+
+    // DON'T TOUCH UNLESS YOU KNOW WHAT YOU'RE DOING
+    // Dynamically changes hearts so they correctly represent the percentage of health left
+    // Should work with any number of hearts and any amount of health/maxHealth so we can have permanent health upgrades
+    // With 5 hearts and 100 health, each heart would represent 20 HP, and the 5 states on each heart would represent 4 HP each
+    public void UpdateHealth(float health, float maxHealth)
+    {
+        float fractionHealth = maxHealth / hearts.Length; // Gets the fraction of health that each heart will contain
+        float heartBase = 0.0f;
+        for(int i = 0; i < hearts.Length; ++i)
+        {
+            hearts[i].GetComponent<Animator>().SetFloat("PercentHealth", (health - heartBase) / fractionHealth); // Passes the percentage of each heart's "fraction" of health on to the animator component to change heart's state
+            heartBase += fractionHealth; // Increases each heart's minimum value by the fraction each contains
+        }
+    }
+
+    public void UpdateScore(float score)
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
--- Group Project/Assets/scripts/Michael Scripts/Globals.cs
+++ Group Project/Assets/scripts/Michael Scripts/Globals.cs
@@ -6,17 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class Globals : MonoBehaviour
 {
-    private static float playerHealth; //Private because it will need to be changed by public function changePlayerHealth(float change);
-    private float maxHealth;
+    private static float playerHealth = 100; //Private because it will need to be changed by public function changePlayerHealth(float change);
+    private static float maxHealth = 100;
+    private static int playerScore = 0;
     public GameObject Player; //Insert player prefab here so that we can access public functions.
+    public GameObject UICanvas;
     //public Slider playerHealthSlider; //Players health will appear with this slider
     // Start is called before the first frame update
 
     void Start()
     {
-        playerHealth = 100; //100 to start
-        maxHealth = 100; //100 is max
-        
 
     }
 
@@ -36,18 +35,46 @@ public class Globals : MonoBehaviour
     {
         playerHealth += change; //health will add change to itself
         Debug.Log("Health is " + returnHealth());
+        if(UICanvas != null)
+        {
+            UICanvas.GetComponent<UI>().UpdateHealth(playerHealth, maxHealth);
+        } else
+        {
+            Debug.Log("You forgot to hook in the UI Canvas to your Globals script!");
+        }
     }
 
+    // The static function may cause issues with the UI. If you need to use it, let me know and I will try to fix it -Phillip
     public static void changePlayerHealthStatic(float change)
     {
         playerHealth += change; //This is a static version of the above function so that we can change the health.
         Debug.Log("Health is " + returnHealth());
     }
 
+    public void changePlayerScore(int change) //Public function that can be call by other scripts. 
+    {
+        playerScore += change; //Score will add change to itself
+        Debug.Log("Score is " + returnScore());
+        if (UICanvas != null)
+        {
+            UICanvas.GetComponent<UI>().UpdateScore(playerScore);
+        }
+        else
+        {
+            Debug.Log("You forgot to hook in the UI Canvas to your Globals script!");
+        }
+    }
+
     private static float returnHealth() //Static function that allows only this Script to return the player Health
     {
         return playerHealth;
     }
+
+    private static int returnScore()
+    {
+        return playerScore;
+    }
+
     void Quit() // Exit the application
     {
 #if UNITY_EDITOR
