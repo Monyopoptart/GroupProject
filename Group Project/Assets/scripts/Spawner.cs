8703e4d88a8d4d1fcc61b3d65ace04719629020d
Riverway14 <57182700+Riverway14@users.noreply.github.com>
2019-12-04 10:53:39 -0500

    did a spwaner

---
+++ Group Project/Assets/scripts/Spawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float speed = 10;
+    public float lifeTime = 3;
+    public GameObject prefabToSpawn;
+    public float spawnDelay = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (prefabToSpawn!=null)
+        {
+            if(col.gameObject.tag == Player)
+
+        }
+    }
+
+
+    public void Spawn()
+    {
+        GameObject spawnedObject = Instantiate(prefabToSpawn,
+            transform.position, transform.rotation);
+        spawnedObject.GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle * 50);
+        Invoke("Spawn", spawnDelay);
+    }
+    IEnumerator Spawn()
+    {
+        GameObject spawnedObject = Instantiate(prefabToSpawn,
+            transform.position, transform.rotation);
+        spawnedObject.GetComponent<Rigidbody2D>().AddForce(x.insideUnitCircle * 50);
+
+    }
+}
