c5cd2fb42cded40fe5db5185d4d55d963f2b9ec1
Phillip Murphy <56118445+philliprmurphy@users.noreply.github.com>
2019-12-05 17:07:32 -0500

    Mostly working grapple, a bit buggy

---
+++ Group Project/Assets/Scripts/GrapplingClaw.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class GrapplingClaw : MonoBehaviour
+{
+    public float maxThrowTime = 2.0f;
+    bool throwFinish = false;
+    bool throwReset = true;
+    GrapplingHook hook;
+
+    private void Start()
+    {
+        hook = gameObject.GetComponentInParent<GrapplingHook>();
+        Invoke("GrappleTimeout", maxThrowTime);
+    }
+    
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player" || col.gameObject.tag == "Enemy") // When hitting the player or enemy
+        {
+            hook.ResetGrapple();
+        }
+        else // When hitting another surface
+        {
+            hook.FreezeGrapple();
+            hook.GrapplePull();
+            throwFinish = true;
+        }
+    }
+
+    // Indicates the throw has begun
+    public void ThrowInitiated()
+    {
+        throwReset = false;
+    }
+
+    // Resets the state of the claw to indicate it has been reset
+    public void ResetAttachment()
+    {
+        throwFinish = false;
+        throwReset = true;
+        Debug.Log("Detached");
+    }
+
+    // Resets the hook if it is being thrown and hasn't hit anything
+    private void GrappleTimeout()
+    {
+        Debug.Log("Attempting grapple throw timeout");
+        if (!throwFinish && !throwReset)
+        {
+            hook.ResetGrapple();
+        }
+    }
+}
--- Group Project/Assets/Scripts/GrapplingHook.cs
+++ Group Project/Assets/Scripts/GrapplingHook.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
 {
     public float grappleForce = 1000.0f;
+    public float pullForce = 1000.0f;
+    public float maxPullTime = 2.0f;
     public float ropeWidth = 0.05f;
-    bool readyToFire = false, firing = false;
-    Vector2 mousePos, objPos;
+    bool readyToFire = false, firing = false, pulling = false;
     float mouseAngle;
+    Vector2 mousePos, objPos, direction;
+    Vector2 initGrapplePos;
+    Quaternion initGrappleRot;
     Rigidbody2D playerRB, grapplingRB;
     GameObject grapple;
     CircleCollider2D grapplingCol;
     SpriteRenderer grapplingSR;
+    GrapplingClaw claw;
     LineRenderer line;
 
     // Start is called before the first frame update
@@ -23,6 +26,7 @@ public class GrapplingHook : MonoBehaviour
         grapplingRB = GetComponentInChildren<Rigidbody2D>();
         grapplingCol = GetComponentInChildren<CircleCollider2D>();
         grapplingSR = GetComponentInChildren<SpriteRenderer>();
+        claw = GetComponentInChildren<GrapplingClaw>();
         grapple = grapplingRB.gameObject;
         grapplingRB.isKinematic = true;
         grapplingCol.enabled = false;
@@ -43,15 +47,19 @@ public class GrapplingHook : MonoBehaviour
             {
                 readyToFire = true;
                 grapplingSR.enabled = true;
+                line.enabled = true;
             }
             else if (Input.GetAxisRaw("Fire3") == 0) // Release fire
             {
                 if (readyToFire)
                 {
                     readyToFire = false;
+                    firing = true;
                     grapplingRB.isKinematic = false;
                     grapplingCol.enabled = true;
                     grapplingRB.AddRelativeForce(new Vector2(grappleForce, grappleForce));
+                    grapple.transform.parent = null; // Removes gameObject as parent
+                    claw.ThrowInitiated();
                 }
             }
 
@@ -65,18 +73,64 @@ public class GrapplingHook : MonoBehaviour
             Quaternion targetRotation = Quaternion.AngleAxis(Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5.0f * Time.deltaTime);
         }
+        // Pulls
+        else if (pulling)
+        {
+            direction = grapple.transform.position;
+            direction.x -= transform.position.x;
+            direction.y -= transform.position.y;
+            direction.Normalize();
+            playerRB.AddForce(direction * pullForce);
+        }
 
         // Drawing line to grappling hook
         if(grapplingSR.enabled)
         {
-            line.SetPosition(0, gameObject.transform.position);
+            line.SetPosition(0, transform.position);
             line.SetPosition(1, grapple.transform.position);
+        }
+
+    }
+
+    // Freezes grapple in place
+    public void FreezeGrapple()
+    {
+        grapplingRB.isKinematic = true;
+        grapplingRB.velocity = Vector2.zero;
+        grapplingRB.angularVelocity = 0;
+    }
+
+    // Activates pulling the player towards grapple
+    public void GrapplePull()
+    {
+        pulling = true;
+        Invoke("GrappleTimeout", maxPullTime);
+    }
+
+    // Resets the grapple if it is still pulling
+    void GrappleTimeout()
+    {
+        if (pulling)
+        {
+            ResetGrapple();
         }
     }
 
-    // Changes the firing state
-    public void SetFiring(bool enabled)
+    // Resets the grapple to where it was
+    public void ResetGrapple()
     {
-        firing = enabled;
+        grapple.transform.parent = transform; // Resets the parent 
+        readyToFire = false;
+        firing = false;
+        pulling = false;
+        grapplingRB.isKinematic = true;
+        grapplingCol.enabled = false;
+        grapplingSR.enabled = false;
+        grapple.transform.localPosition = new Vector2(0 + 2, 0);
+        grapple.transform.localRotation = Quaternion.Euler(0, 0, -45);
+        claw.ResetAttachment();
+        Debug.Log("Successful Reset");
+
+        line.enabled = false;
     }
 }
