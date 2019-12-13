7236bd81512411afeb19b78208411efbb0e78068
Monyopoptart <Michaellouissheen@gmail.com>
2019-12-13 10:37:50 -0800

    Added color changing Goblin to level 2
    IT's super spazzy

---
+++ Group Project/Assets/scripts/Michael Scripts/ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private SpriteRenderer thisThing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisThing = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float randomColorx = Random.Range(0f, 1f);
+        float randomColory = Random.Range(0f, 1f);
+        float randomColorz = Random.Range(0f, 1f);
+        thisThing.color = new Color(randomColorx, randomColory, randomColorz); //This will cause his color to change seizure like
+    }
+}
